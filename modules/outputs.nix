8f0a5c6b2eba549defa5edf498505e2d3fc3a1b7
greyxp1 <greyxp999@gmail.com>
2026-06-24 07:51:37 -0400

    added home manader

--- modules/outputs.nix
+++ modules/outputs.nix
@@ -1,5 +1,5 @@
-_: {
-  perSystem = {pkgs, system, ...}: let
+{inputs, self, ...}: {
+  perSystem = {lib, pkgs, system, ...}: let
     sources = import ./sources.nix;
 
     version =
@@ -26,6 +26,42 @@ _: {
           ;
       };
     };
+
+    moduleCheck = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        inputs.home-manager.nixosModules.home-manager
+        self.nixosModules.helium
+        {
+          boot.loader.grub.devices = ["nodev"];
+          fileSystems."/" = {
+            device = "none";
+            fsType = "tmpfs";
+          };
+          system.stateVersion = "26.05";
+
+          users.users.helium-test = {
+            isNormalUser = true;
+            home = "/home/helium-test";
+          };
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.helium-test = {
+              imports = [self.homeModules.helium];
+              home.stateVersion = "26.05";
+              programs.helium = {
+                enable = true;
+                defaultBrowser = true;
+                extraPolicies.HomepageLocation = "https://helium.computer";
+                preferences.browser.show_home_button = true;
+              };
+            };
+          };
+        }
+      ];
+    };
   in {
     packages = {
       default = helium;
@@ -37,17 +73,9 @@ _: {
       helium = app;
     };
 
-    devShells.default = pkgs.mkShell {
-      packages = [
-        helium
-        pkgs.nix-update
-      ];
-    };
-
-    formatter = pkgs.nixfmt-tree;
-
-    checks = {
-      build = helium;
+    devShells.default = pkgs.mkShell {packages = [helium pkgs.nix-update];};
+    checks = {build = helium;} // lib.optionalAttrs pkgs.stdenv.isLinux {
+      module = moduleCheck.config.system.build.toplevel;
     };
   };
 }
